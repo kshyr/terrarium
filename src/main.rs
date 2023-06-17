3341d184987f7fb8759dc27e5fabe83cc9b6df4b
deko95 <ks1379@scarletmail.rutgers.edu>
2023-06-16 22:27:24 -0400

    food collision

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 use bevy::{
     core_pipeline::clear_color::ClearColorConfig, prelude::*, sprite::MaterialMesh2dBundle,
     window::PrimaryWindow,
@@ -13,6 +15,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_ants)
+        .add_startup_system(spawn_food)
         .add_system(move_ants)
         .add_system(update_ant_direction)
         .run();
@@ -23,6 +26,9 @@ pub struct Ant {
     direction: Vec2,
     search_timer: Timer,
 }
+
+#[derive(Component)]
+pub struct Food {}
 
 fn spawn_ants(
     mut commands: Commands,
@@ -46,6 +52,29 @@ fn spawn_ants(
     }
 }
 
+fn spawn_food(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    const FOOD_SIZE: u32 = 32;
+    for _ in 0..FOOD_SIZE {
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(2.0).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::OLIVE)),
+                transform: Transform::from_translation(Vec3::new(
+                    random_range(-200.0, -210.0),
+                    random_range(-200.0, -210.0),
+                    0.,
+                )),
+                ..Default::default()
+            },
+            Food {},
+        ));
+    }
+}
+
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle {
         camera_2d: Camera2d {
@@ -56,13 +85,22 @@ fn spawn_camera(mut commands: Commands) {
 }
 
 fn move_ants(
-    mut ants_query: Query<(&mut Transform, &mut Ant)>,
+    mut ants_query: Query<(&mut Transform, &mut Ant), Without<Food>>,
+    mut food_query: Query<(&mut Transform, &mut Food), Without<Ant>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
     time: Res<Time>,
 ) {
     let window = window_query.get_single().unwrap();
     for (mut transform, mut ant) in ants_query.iter_mut() {
         let translation = &mut transform.translation;
+        for food in food_query.iter() {
+            let food_translation = food.0.translation;
+            if (food_translation.x - translation.x).abs() < 1.0
+                && (food_translation.y - translation.y).abs() < 1.0
+            {
+                println!("Touched food!");
+            }
+        }
         translation.x += ant.direction.x * ANT_SPEED * time.delta_seconds();
         translation.y += ant.direction.y * ANT_SPEED * time.delta_seconds();
         translation.x = translation
