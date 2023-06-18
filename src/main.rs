3b714747517e9f74c54724b5368f6a94d301a4d4
deko95 <ks1379@scarletmail.rutgers.edu>
2023-06-18 00:38:02 -0400

    hacky way to make ants go back and forth between food and home

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ const SEARCH_FACTOR: f32 = 0.2;
 const TRAIL_INTERVAL: f32 = 0.01;
 const TRAIL_LIFESPAN: f32 = 10.0;
 const FOOD_DISTANCE: f32 = 150.0;
+const HOME: Vec2 = Vec2::new(0.0, 0.0);
 
 fn main() {
     App::new()
@@ -30,6 +31,7 @@ pub struct Ant {
     direction: Vec2,
     search_timer: Timer,
     trail_timer: Timer,
+    found_food: bool,
 }
 
 #[derive(Component)]
@@ -51,13 +53,14 @@ fn spawn_ants(
             MaterialMesh2dBundle {
                 mesh: meshes.add(shape::Circle::new(ANT_SIZE).into()).into(),
                 material: materials.add(ColorMaterial::from(Color::BEIGE)),
-                transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+                transform: Transform::from_translation(Vec3::new(HOME.x, HOME.y, 10.)),
                 ..Default::default()
             },
             Ant {
                 direction,
                 search_timer: Timer::from_seconds(0.01, TimerMode::Repeating),
                 trail_timer: Timer::from_seconds(TRAIL_INTERVAL, TimerMode::Repeating),
+                found_food: false,
             },
         ));
     }
@@ -69,6 +72,38 @@ fn spawn_food(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     const FOOD_SIZE: u32 = 32;
+    for _ in 0..FOOD_SIZE {
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(2.0).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::OLIVE)),
+                transform: Transform::from_translation(Vec3::new(
+                    random_range(-200.0, -210.0),
+                    random_range(-200.0, -210.0),
+                    0.,
+                )),
+                ..Default::default()
+            },
+            Food {},
+        ));
+    }
+
+    for _ in 0..FOOD_SIZE {
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(2.0).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::OLIVE)),
+                transform: Transform::from_translation(Vec3::new(
+                    random_range(-100.0, -110.0),
+                    random_range(100.0, 110.0),
+                    0.,
+                )),
+                ..Default::default()
+            },
+            Food {},
+        ));
+    }
+
     for _ in 0..FOOD_SIZE {
         commands.spawn((
             MaterialMesh2dBundle {
@@ -100,7 +135,7 @@ fn move_ants(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut ants_query: Query<(&mut Transform, &mut Ant), Without<Food>>,
-    mut food_query: Query<(&mut Transform, &mut Food), Without<Ant>>,
+    mut food_query: Query<(&mut Transform, Entity, &mut Food), Without<Ant>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
     time: Res<Time>,
 ) {
@@ -108,26 +143,30 @@ fn move_ants(
 
     for (mut transform, mut ant) in ants_query.iter_mut() {
         let translation = &mut transform.translation;
-        // for mut food in food_query.iter_mut() {
-        //     let mut food_translation = &mut food.0.translation;
-        //     if (food_translation.x - translation.x).abs() < 1.0
-        //         && (food_translation.y - translation.y).abs() < 1.0
-        //     {
-        //         food_translation.x -= 50.0 * time.delta_seconds();
-        //         food_translation.y -= 50.0 * time.delta_seconds();
-        //     }
-        // }
-        //
+        for mut food in food_query.iter_mut() {
+            let mut food_translation = &mut food.0.translation;
+            if (food_translation.x - translation.x).abs() < 1.0
+                && (food_translation.y - translation.y).abs() < 1.0
+            {
+                ant.found_food = true;
+                commands.entity(food.1).despawn();
+            }
+        }
+
         ant.trail_timer.tick(time.delta());
         if ant.trail_timer.finished() {
             commands.spawn((
                 MaterialMesh2dBundle {
                     mesh: meshes.add(shape::Circle::new(1.0).into()).into(),
-                    material: materials.add(ColorMaterial::from(Color::CRIMSON)),
+                    material: materials.add(ColorMaterial::from(if ant.found_food {
+                        Color::SEA_GREEN
+                    } else {
+                        Color::CRIMSON
+                    })),
                     transform: Transform::from_translation(Vec3::new(
                         translation.x,
                         translation.y,
-                        0.,
+                        1.0,
                     )),
                     ..Default::default()
                 },
@@ -167,7 +206,7 @@ fn update_trails(
     for (mut transform, mut trail, entity, mut color) in trail_query.iter_mut() {
         trail.intensity -= 1.0 / TRAIL_LIFESPAN * time.delta_seconds();
         let mut color_mat = materials.get_mut(&color).unwrap();
-        color_mat.color = Color::rgba(0.8, 0.3, 0.3, trail.intensity);
+        color_mat.color.set_a(trail.intensity);
 
         if trail.intensity <= 0.0 {
             commands.entity(entity).despawn();
@@ -176,6 +215,7 @@ fn update_trails(
 }
 
 fn update_ant_direction(
+    mut commands: Commands,
     mut ants_query: Query<(&mut Transform, &mut Ant), Without<Food>>,
     mut food_query: Query<(&mut Transform, &mut Food), Without<Ant>>,
     time: Res<Time>,
@@ -184,11 +224,22 @@ fn update_ant_direction(
         let ant_pos = transform.translation;
         // let direction = Vec2::new(random_range(-1.0, 1.0), random_range(-1.0, 1.0)).normalize();
 
+        if ant.found_food {
+            if (HOME.x - ant_pos.x).abs() < 4.0 && (HOME.y - ant_pos.y).abs() < 4.0 {
+                ant.found_food = false;
+                ant.direction *= -1.0;
+            } else {
+                ant.direction = Vec2::new(HOME.x - ant_pos.x, HOME.y - ant_pos.y).normalize();
+            }
+        }
+
         ant.search_timer.tick(time.delta());
         if ant.search_timer.finished() {
             for mut food in food_query.iter_mut() {
                 let food_pos = food.0.translation;
-                if (food_pos.x - ant_pos.x).abs() < FOOD_DISTANCE
+
+                if !ant.found_food
+                    && (food_pos.x - ant_pos.x).abs() < FOOD_DISTANCE
                     && (food_pos.y - ant_pos.y).abs() < FOOD_DISTANCE
                 {
                     ant.direction =
